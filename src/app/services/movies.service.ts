2aa16aa24548899dfe239c7d28f8b4b4feee17fb
César Gonzalez <cedgo1997@gmail.com>
2020-08-03 03:32:04 -0400

    getPopular method created

--- src/app/services/movies.service.ts
+++ src/app/services/movies.service.ts
@@ -1,14 +1,18 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class MoviesService {
+  private apiKey: string = "527d31593527d0b6d65cca79c0370e36";
+  private movieUrl: string = "https://api.themoviedb.org/3";
 
+  constructor(private http: HttpClient) {}
 
-  private apiKey:string = '527d31593527d0b6d65cca79c0370e36';
-  private movieUrl:string = 'https://api.themoviedb.org/3'
-
-  constructor( private http: HttpClient ) { }
+  getPopular() {
+    return this.http.get(
+      `${this.movieUrl}/discover/movie?sort_by=popularity.desc&api_key=${this.apiKey}`
+    );
+  }
 }
