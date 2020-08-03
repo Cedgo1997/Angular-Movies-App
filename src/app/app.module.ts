349bd1b8af6678010c20be426f2ef08b39c06bf9
César Gonzalez <cedgo1997@gmail.com>
2020-08-03 03:03:55 -0400

    routerlink on navbar added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import  { HttpClientModule } from '@angular/common/http';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+/* my MODULES */
+import  { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+
+/* my COMPONENTS */
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
 import { SearchComponent } from './components/search/search.component';
@@ -20,7 +24,8 @@ import { MoviesComponent } from './components/movies/movies.component';
   imports: [
     BrowserModule,
     HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
